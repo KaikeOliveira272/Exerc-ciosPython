d209399ab3be3263ae87d75d512d1c86daab0786
KaikeOliveira272 <oliveirakaike272@gmail.com>
2025-02-25 14:08:46 -0300

    Exercício inversão de String em Python

---
+++ String invertida.py
@@ -0,0 +1,10 @@
+
+palavra = input("Digite o seu nome: ")
+invertido = palavra [::-1]
+
+
+if palavra == invertido:
+    print("Essa palavra é um palíndromo")
+
+else:
+    print("Essa palavra NÃO é um palíndromo")
